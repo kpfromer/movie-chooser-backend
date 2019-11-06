b4973ac761c76900001cf18e9e609238488853bc
Kyle Pfromer <kpfromer2@gmail.com>
2019-11-06 11:39:51 -0700

    Fix mongo uri env

--- src/config.js
+++ src/config.js
@@ -3,8 +3,7 @@ import convict from 'convict';
 export const config = convict({
   port: { env: 'PORT', default: 3001 },
   mongo: {
-    env: 'MONGO_URI',
-    uri: { default: 'mongodb://localhost:27017/movie-chooser-graphql' }
+    uri: { env: 'MONGO_URI', default: 'mongodb://localhost:27017/movie-chooser-graphql' }
   },
   auth: { secret: { default: 'jwt-secret' } },
   apiKey: { default: 'bad-api-key', env: 'API_KEY' }
